2811c7f9896aa28ee8aa540a6cb5acc18bbc2339
nouryy17 <dacinour16@gmail.com>
2024-07-05 10:00:39 +0200

    add rs mobile-small

--- the project/js/script.js
+++ the project/js/script.js
@@ -1,48 +1,15 @@
-
-let svg =document.querySelectorAll("header .svg svg")
-let list =document.querySelector("header ul")
-
+let svg =document.querySelector(".page-header .svg svg")
+console.log(document.querySelector(".page-header .svg "))
+let list =document.querySelector(".page-header nav > div")
 function toggleActivClass(clickEle) {
-    let svgActiv=document.querySelectorAll("header .svg svg.activ")
+    let svgActiv=document.querySelectorAll("nav .svg svg.activ")
     svgActiv.forEach((ele) =>{
         ele.classList.remove("activ")
         list.classList.toggle("activ")
     })
     clickEle.classList.add("activ")
 }
-let arrow=document.querySelectorAll(".Questions .boxs .box svg")
-let box=document.querySelectorAll(".Questions .boxs .box ")
-arrow.forEach(function(ele) {
-
-    ele.addEventListener("click" , function () {
-        ele.classList.toggle("activ")
-        ele.parentElement.parentElement.classList.toggle('activ')
-    })
-}  
-)
 
 
 
-const listItems = document.querySelectorAll('.Features .tow .List li');
-const featureImg = document.querySelector('.Features .tow .info img');
-const featureTitle = document.querySelector('.Features .tow .info h1');
-const featureText = document.querySelector('.Features .tow .info p');
-
-listItems.forEach(function (item) {
-
-    
-    item.addEventListener("click",function () {
-        listItems.forEach(function (item) {
-            item.classList.remove("activ")
-        })
-        item.classList.add("activ")
-        const title = item.getAttribute('data-title');
-        const text = item.getAttribute('data-text');
-        const imgSrc = item.getAttribute('data-img');
-
-        featureTitle.textContent = title;
-        featureText.textContent = text;
-        featureImg.src = imgSrc;
-    })
-})
 
